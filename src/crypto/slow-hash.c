2184d1b717728f69d99c17fdef6d96b1ebf0c750
Brandon Lehmann <brandonlehmann@gmail.com>
2018-10-07 17:32:11 -0400

    Adding `PAGE_SIZE` as this is more accurate for how this is actually handled.

--- src/crypto/slow-hash.c
+++ src/crypto/slow-hash.c
@@ -45,6 +45,9 @@
 #define AES_KEY_SIZE           32
 #define INIT_SIZE_BLK          8
 #define INIT_SIZE_BYTE         (INIT_SIZE_BLK * AES_BLOCK_SIZE)
+#define PAGE_SIZE              2097152
+
+
 
 // Standard Cryptonight Definitions
 #define MEMORY                 2097152 // 2MB scratchpad 2^21
@@ -143,7 +146,7 @@ extern int aesb_pseudo_round(const uint8_t *in, uint8_t *out, const uint8_t *exp
 #define ASM __asm
 #endif
 
-#define TOTALBLOCKS (MEMORY / AES_BLOCK_SIZE)
+#define TOTALBLOCKS (PAGE_SIZE / AES_BLOCK_SIZE)
 
 #define U64(x) ((uint64_t *) (x))
 #define R128(x) ((__m128i *) (x))
@@ -502,15 +505,15 @@ void slow_hash_allocate_state(void)
 
 #if defined(_MSC_VER) || defined(__MINGW32__)
     SetLockPagesPrivilege(GetCurrentProcess(), TRUE);
-    hp_state = (uint8_t *) VirtualAlloc(hp_state, MEMORY, MEM_LARGE_PAGES |
+    hp_state = (uint8_t *) VirtualAlloc(hp_state, PAGE_SIZE, MEM_LARGE_PAGES |
                                         MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 #else
 #if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || \
   defined(__DragonFly__)
-    hp_state = mmap(0, MEMORY, PROT_READ | PROT_WRITE,
+    hp_state = mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE,
                     MAP_PRIVATE | MAP_ANON, 0, 0);
 #else
-    hp_state = mmap(0, MEMORY, PROT_READ | PROT_WRITE,
+    hp_state = mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE,
                     MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, 0, 0);
 #endif
     if(hp_state == MAP_FAILED)
@@ -520,7 +523,7 @@ void slow_hash_allocate_state(void)
     if(hp_state == NULL)
     {
         hp_allocated = 0;
-        hp_state = (uint8_t *) malloc(MEMORY);
+        hp_state = (uint8_t *) malloc(PAGE_SIZE);
     }
 }
 
@@ -540,7 +543,7 @@ void slow_hash_free_state(void)
 #if defined(_MSC_VER) || defined(__MINGW32__)
         VirtualFree(hp_state, 0, MEM_RELEASE);
 #else
-        munmap(hp_state, MEMORY);
+        munmap(hp_state, PAGE_SIZE);
 #endif
     }
 
@@ -767,7 +770,7 @@ union cn_slow_hash_state
  */
 #include <arm_neon.h>
 
-#define TOTALBLOCKS (MEMORY / AES_BLOCK_SIZE)
+#define TOTALBLOCKS (PAGE_SIZE / AES_BLOCK_SIZE)
 
 #define state_index(x,div) (((*((uint64_t *)x) >> 4) & (TOTALBLOCKS /(div) - 1)) << 4)
 #define __mul() __asm__("mul %0, %1, %2\n\t" : "=r"(lo) : "r"(c[0]), "r"(b[0]) ); \
@@ -930,7 +933,7 @@ void cn_slow_hash(const void *data, size_t length, char *hash, int light, int va
 	size_t lightFlag = (light ? 2 : 1);
 	
     RDATA_ALIGN16 uint8_t expandedKey[240];
-    RDATA_ALIGN16 uint8_t hp_state[MEMORY];
+    RDATA_ALIGN16 uint8_t hp_state[PAGE_SIZE];
 
     uint8_t text[INIT_SIZE_BYTE];
     RDATA_ALIGN16 uint64_t a[2];
@@ -1151,10 +1154,10 @@ void cn_slow_hash(const void *data, size_t length, char *hash, int light, int va
     };
 
 #ifndef FORCE_USE_HEAP
-    uint8_t long_state[MEMORY];
+    uint8_t long_state[PAGE_SIZE];
 #else
     uint8_t *long_state = NULL;
-    long_state = (uint8_t *)malloc(MEMORY);
+    long_state = (uint8_t *)malloc(PAGE_SIZE);
 #endif
 
     if (prehashed) {
@@ -1185,7 +1188,7 @@ void cn_slow_hash(const void *data, size_t length, char *hash, int light, int va
 
     for(i = 0; i < aes_rounds; i++)
     {
-      #define MASK(div) ((uint32_t)(((MEMORY / AES_BLOCK_SIZE) / (div) - 1) << 4))
+      #define MASK(div) ((uint32_t)(((PAGE_SIZE / AES_BLOCK_SIZE) / (div) - 1) << 4))
       #define state_index(x,div) ((*(uint32_t *) x) & MASK(div))
 
       // Iteration 1
@@ -1323,7 +1326,8 @@ void cn_slow_hash(const void *data, size_t length, char *hash, int light, int va
   size_t init_rounds = (light ? CN_LIGHT_INIT : CN_INIT);
   size_t aes_rounds = (light ? ITER_Light_Divided : ITER_Divided);
   size_t aes_init = (light ? CN_LIGHT_AES_INIT : CN_AES_INIT);
-  uint8_t long_state[MEMORY];
+  
+  uint8_t long_state[PAGE_SIZE];
   union cn_slow_hash_state state;
   uint8_t text[INIT_SIZE_BYTE];
   uint8_t a[AES_BLOCK_SIZE];
